423116aa302bd3af1907684812e3fbe324651097
Vlad Rusakov <vlad.rusakov.1996@mail.ru>
2020-02-29 20:17:15 +0300

    Platformer w/ generating stuff completed

--- coin/coin.gd
+++ coin/coin.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
 class_name Coin
-
-
+var anchor = null
 var taken = false
+
+func _process(delta):
+	if self.position.x < -100:
+		self.queue_free()
+	if self.anchor != null and self.position != null:
+		self.position = self.anchor.position + Vector2(0, -30)
 
 func _on_coin_body_enter(body):
 	if not taken and body is Player:
+		var player = (body as Player)
 		($Anim as AnimationPlayer).play("taken")
 		taken = true
+		player.score += 1
